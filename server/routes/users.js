93cdd86552be40448a9235ee6291ce7e927d2590
Ganesh5557 <ganesh.pandey5557@gmail.com>
2020-04-30 18:22:12 -0400

    up

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/User");
+const User = require("../models/User");
 
-const { auth } = require("../middleware/auth");
+const auth = require("../middleware/auth");
 
 const async = require('async');
 
@@ -19,7 +19,7 @@ router.get("/auth", auth, (req, res) => {
         name: req.user.name,
         lastname: req.user.lastname,
         role: req.user.role,
-        image: req.user.image,
+        image: req.user.image
     });
 });
 
