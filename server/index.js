aa681e303e1221e9c8051aa767ddd990fe027f33
Ganesh5557 <ganesh.pandey5557@gmail.com>
2020-04-30 12:56:27 -0400

    up

--- server/index.js
+++ server/index.js
@@ -35,12 +35,12 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+  // console.log(`Server Running at ${port}`)
 });
