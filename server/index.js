df16ae5d0e60fc07e6252106fb1ce32baf188ca0
Ganesh5557 <ganesh.pandey5557@gmail.com>
2020-04-30 12:48:03 -0400

    up

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cors = require('cors')
+const cors = require('cors');
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require('../models/User');
+const { User } = require("../models/User");
 
 const { auth } = require("../middleware/auth");
+
 const async = require('async');
 
 //=================================
@@ -65,8 +66,8 @@ router.get("/logout", auth, (req, res) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).send({
             success: true
-        });
-    });
-});
+        })
+    })
+})
 
 module.exports = router;
